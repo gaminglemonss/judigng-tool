c55d96a893a14daa33a98450e56ecaa9f94eb7cb
Lemon Games <124937565+gaminglemonss@users.noreply.github.com>
2023-02-16 19:44:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ design = input("Input design [0 - 10]");
 input("Design: " + design + "/10", "");
 
 efficiency = input("Input efficiency  [0 - 10]");
-input"Efficiency: " + efficiency + "/10". "");
+input("Efficiency: " + efficiency + "/10". "");
 
 complexity = input("Input complexity [0 - 10]");
 input("Complexity: " + complexity + "/10", "");
