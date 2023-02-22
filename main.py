1fb9149977060b40b9055ae8018a45a02046ca16
Lemon Games <124937565+gaminglemonss@users.noreply.github.com>
2023-02-22 12:12:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-creativity = input("Input creativity [0 - 10]");
+creativity = input("Input creativity [0 - 10]\n");
 print("Creativity: " + creativity + "/10", "");
 
-design = input("Input design [0 - 10]");
+design = input("Input design [0 - 10]\n");
 print("Design: " + design + "/10", "");
 
-efficiency = input("Input efficiency  [0 - 10]");
+efficiency = input("Input efficiency  [0 - 10]\n");
 print("Efficiency: " + efficiency + "/10". "");
 
-complexity = input("Input complexity [0 - 10]");
+complexity = input("Input complexity [0 - 10]\n");
 print("Complexity: " + complexity + "/10", "");
